195d8a2a714ab16fbc2fc46653b121850e9c9126
Eduardo Costa de Sousa <105511756+Jackvs720v@users.noreply.github.com>
2025-02-20 20:37:29 -0300

    Update ex3.dart

--- aula3Exercicios/ex3.dart
+++ aula3Exercicios/ex3.dart
@@ -1,9 +1,17 @@
 import 'dart:io';
-
+ 
 void main(){
-  
+    double slB, desconto, bonificacao;
+    slB = double.parse(stdin.readLineSync()!);
+    desconto = double.parse(stdin.readLineSync()!);
+    bonificacao = double.parse(stdin.readLineSync()!);
+ 
+  salarioLiquido(slB,desconto, bonificacao);
+ 
 }
-
+ 
 void salarioLiquido(slB,desconto,bonificacao){
-
+    double slL;
+    slL = slB - (desconto+bonificacao);
+    print("O salário liquido é de $slL");
 }
